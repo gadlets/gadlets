6ff70507e2d122a0ae91a032a2af810b2ea323d5
Magnus Sandberg <emsa@gadlets.org>
2011-06-19 20:27:08 +0200

    Added GadletInstance and GadletInstanceRepository

---
+++ gadlets-core/src/main/java/org/gadlets/core/GadletInstance.java
@@ -0,0 +1,15 @@
+package org.gadlets.core;
+
+public class GadletInstance {
+
+	private String path;
+
+	public GadletInstance(String path) {
+		this.path = path;
+	}
+
+	public String getPath() {
+		return path;
+	}
+	
+}
--- gadlets-core/src/main/java/org/gadlets/tag/GadletsGenerator.java
+++ gadlets-core/src/main/java/org/gadlets/tag/GadletsGenerator.java
@@ -3,16 +3,16 @@ package org.gadlets.tag;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.net.URL;
+import java.util.List;
 import java.util.Random;
+
+import org.gadlets.core.GadletInstance;
+import org.gadlets.core.GadletInstanceRepository;
 
 public class GadletsGenerator {
 
 	public static String generateGadletInclude() throws IOException {
-
 		
-		URL resource = GadletsGenerator.class.getClassLoader().getResource("org/gadlets/tag/test1.xhtml");
-
 		File file = File.createTempFile("gadlets_" + new Random().nextInt(), ".xhtml");
 		FileWriter fw = new FileWriter(file);
 		fw.write("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">");
@@ -21,7 +21,10 @@ public class GadletsGenerator {
 		
 		fw.write("GEN START: " + file.getAbsolutePath() + "<br/>");
 		
-		fw.write("<ui:include src=\""+ resource.toString()+" \"/>");
+		List<GadletInstance> instances = GadletInstanceRepository.getInstances();
+		for (GadletInstance gadletInstance : instances) {
+			fw.write("<ui:include src=\""+ gadletInstance.getPath() +" \"/>");			
+		}
 		
 		fw.write("<br/> GEN END");
 		
